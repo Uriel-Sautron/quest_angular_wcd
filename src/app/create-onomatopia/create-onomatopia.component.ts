97915a1ebf45ddf6ef2d38f3d8c575f8463b38c2
Uriel SAUTRON <uriel.sautron@gmail.com>
2022-10-25 19:57:58 +0200

    Quest 8 done

---
+++ src/app/create-onomatopia/create-onomatopia.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopia',
+  templateUrl: './create-onomatopia.component.html',
+  styleUrls: ['./create-onomatopia.component.css'],
+})
+export class CreateOnomatopiaComponent implements OnInit {
+  constructor() {}
+
+  newOnomatopia: string = '';
+
+  @Output()
+  sendOnomatopiaToParent: EventEmitter<string> = new EventEmitter();
+
+  createOnomatopia(): void {
+    this.sendOnomatopiaToParent.emit(this.newOnomatopia);
+  }
+
+  ngOnInit(): void {}
+}
